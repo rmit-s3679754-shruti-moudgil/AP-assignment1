0e9cc50ba2750247aa24cb20fa93486766cdafe0
rmit-s3675499-tushar <31883300+rmit-s3675499-tushar@users.noreply.github.com>
2018-03-27 13:17:28 +1100

    added connection.java

---
+++ connection.java
@@ -0,0 +1,13 @@
+public class connection {
+	public person p1;
+	public person p2;
+	public String typeOfConn ;
+
+	
+	public connection (person p11, person p22, String type)
+	{
+		this.p1 = p11;
+		this.p2 = p22;
+		this.typeOfConn = type;
+	}
+}
